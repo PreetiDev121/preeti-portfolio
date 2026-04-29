f949409f57084913fd94a0525a880d2c5480cca4
PreetiDev121 <preetibisht345@gmail.com>
2026-04-29 18:00:23 +0530

    Added deployed code vite update

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// 👉 replace with your actual repo name
 export default defineConfig({
   plugins: [react()],
+  base: '/preeti-portfolio/',
 })
