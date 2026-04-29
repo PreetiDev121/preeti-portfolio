1e7ce5478f3b635bdfb7bfb42bc237f6e739b2f3
PreetiDev121 <preetibisht345@gmail.com>
2026-04-29 19:26:55 +0530

    Added code new

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// 👉 replace with your actual repo name
 export default defineConfig({
   plugins: [react()],
-  base: '/',
 })
